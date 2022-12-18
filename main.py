7876750d7f733423291271d7d7dfdd0ceb2ff50e
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 04:55:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,8 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 
-import st.bokeh_chart
-
 
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
-p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-
-p.line(x, y, legend_label="Trend", line_width=2)
-
-st.bokeh_chart(p)
+st.bokeh_chart(chart_data)
