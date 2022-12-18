90a142fbcc7bdc9fef58ac37e8f77b2241319dd0
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 04:29:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt # required later
-from causalimpact import CausalImpact
+import pyplot as plt
+import numpy as np
 
-import streamlit as st
-
-df = pd.DataFrame({'date': ['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04', '2021-01-05', '2021-01-06'],
-
-                   'value': [1539, 1696, 1427, 1341, 1426, 1471]})
-
-@st.experimental_memo
-def get_pre_post(data, change):  
-    pre_start = min(data.index)
-    pre_end = int(data[data['date'] == change].index.values)
-    post_start = pre_end + 1
-    post_end = max(data.index)
-    pre_period = [pre_start, pre_end]
-    post_period = [post_start, post_end]
-
-    return pre_period, post_period
-
-change = '2021-01-05'
-
-pre_period, post_period = get_pre_post(df, change)
-
-ci = CausalImpact(df.drop(['date'], axis=1), pre_period, post_period, prior_level_sd=None)
-
-ci.plot()
-
-fig = plt.gcf() # to get current figure
-ax = plt.gca() # to get current axes
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
 st.pyplot(fig)
