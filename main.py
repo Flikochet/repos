628c07b924a8a40d3a87f7b129523a454bb1c277
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 04:49:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import streamlit as st
-from bokeh.plotting import figure
+import pandas as pd
+import numpy as np
 
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-
-p.line(x, y, legend_label="Trend", line_width=2)
-
-st.bokeh_chart(p)
+st.line_chart(chart_data)
