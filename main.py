1b73ae06a3240a27777146a1bc388f26e04e5dd6
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 04:36:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 @st.experimental_memo
+
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
