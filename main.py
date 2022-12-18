5fb213b7938f8a0237da8f6d451d0a8c7364de7c
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 04:31:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import matplotlib as plt
+import plotly.express as px
 import numpy as np
 
 arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
+fig, ax = px.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
