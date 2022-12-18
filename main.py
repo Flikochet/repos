3f679405997a991c4b05d21e6ac830dfb34b19e7
Flikochet <120843567+Flikochet@users.noreply.github.com>
2022-12-18 05:00:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+#Import the required Libraries
+
 import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+# Add a title and intro text
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
 
-st.line_chart(chart_data)
+# Create file uploader object
+upload_file = st.file_uploader('Upload a file containing earthquake data')
 
+# Check to see if a file has been uploaded
+if upload_file is not None:
+    # If it has then do the following:
 
+    # Read the file to a dataframe using pandas
+    df = pd.read_csv(upload_file)
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+    # Create a section for the dataframe statistics
+    st.header('Statistics of Dataframe')
+    st.write(df.describe())
 
-st.bokeh_chart(chart_data)
+    # Create a section for the dataframe header
+    st.header('Header of Dataframe')
+    st.write(df.head())
+
+    # Create a section for matplotlib figure
+    st.header('Plot of Data')
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(x=df['Depth'], y=df['Magnitude'])
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Magnitude')
+    
+    st.pyplot(fig)
